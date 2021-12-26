343f7ff80cb119ca488f787a2486729e07e8330c
dimi9595 <dmitry.mindlin@gmail.com>
2021-12-26 21:51:18 +0100

    Adapted package names.

--- slurm/load_mura_dataset.job.sh
+++ slurm/load_mura_dataset.job.sh
@@ -3,4 +3,4 @@ source /media/compute/homes/dmindlin/.bashrc
 conda activate medical-decision-support-system
 cd /media/compute/homes/dmindlin/medical-decision-support-system
 
-python3 -m medical-decision-support-system.mura_pretraining.dataloader.load_dataset
+python3 -m mura_pretraining.dataloader.load_dataset
