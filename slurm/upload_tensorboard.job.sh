8d36b16570454409a2c8c9bd3a36368480243d5c
dimi9595 <dmitry.mindlin@gmail.com>
2021-12-28 14:37:14 +0100

    Adapted logs dir in upload_tensorboard

--- slurm/upload_tensorboard.job.sh
+++ slurm/upload_tensorboard.job.sh
@@ -4,6 +4,6 @@ conda activate medical-decision-support-system
 
 cd /media/compute/homes/dmindlin/medical-decision-support-system
 
-tensorboard dev upload --logdir ../logs \
+tensorboard dev upload --logdir logs \
     --name "Mura Pretraining Board" \
     --description "Comparison of several hyperparameters and pretraining"
