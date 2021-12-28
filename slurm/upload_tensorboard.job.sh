d2064363eada566e3b7a4138c6f029155eb33c58
dimi9595 <dmitry.mindlin@gmail.com>
2021-12-28 13:41:21 +0100

    Added upload tensorboard jobs.

---
+++ slurm/upload_tensorboard.job.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+source /media/compute/homes/dmindlin/.bashrc
+conda activate medical-decision-support-system
+
+cd /media/compute/homes/dmindlin/medical-decision-support-system
+
+tensorboard dev upload --logdir ../logs \
+    --name "Mura Pretraining Board" \
+    --description "Comparison of several hyperparameters and pretraining"
