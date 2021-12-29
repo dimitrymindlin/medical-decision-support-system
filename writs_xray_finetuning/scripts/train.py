0b2b062641b189387ff445ff9f54fbd433c62333
dimi9595 <dmitry.mindlin@gmail.com>
2021-12-29 22:03:13 +0100

    Adapted save_checkpoints path

--- writs_xray_finetuning/scripts/train.py
+++ writs_xray_finetuning/scripts/train.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
 
 import tensorflow as tf
-import tensorflow_addons as tfa
+# import tensorflow_addons as tfa
 from datetime import datetime
 
-from configs.mura_pretraining_config import mura_config
-from mura_pretraining.dataloader.mura_dataset import MuraDataset
-from mura_pretraining.model.mura_model import MuraDenseNet
+from configs.wrist_xray_config import wrist_xray_config
+from writs_xray_finetuning.dataloader import WristXrayDataset
+from writs_xray_finetuning.model.wrist_xray_model import WristXrayDenseNet
 import sys
 
-config = mura_config
+
+config = wrist_xray_config
 # set cli arguments
 for arg in sys.argv:
     if arg == "--use_class_weights":
@@ -17,19 +18,21 @@ for arg in sys.argv:
     elif arg == "--augmentation":
         config["train"]["augmentation"] = True
 
+
 input_shape = (None,
-    config['data']['image_height'],
-    config['data']['image_width'],
-    config['data']['image_channel'])
+               config['data']['image_height'],
+               config['data']['image_width'],
+               config['data']['image_channel'])
 
-dataset = MuraDataset(config)
-
+dataset = WristXrayDataset(config)
 
 # Model Definition
 train_base = config['train']['train_base']
-model = MuraDenseNet(config, train_base=train_base).model()
+model = WristXrayDenseNet(config, train_base=train_base).model()
+if config['train']['use_mura_weights']:
+    model.load_weights("checkpoint/chexnet/best/cp.ckpt") # TODO: Adapt weights path
 
-#TODO: Check if you want HPARAMS with Optimizer and LR
+# TODO: Check if you want HPARAMS with Optimizer and LR
 """optimizer_name = hparams[HP_OPTIMIZER]
 learning_rate = hparams[HP_L_RATE]
 if optimizer_name == "adam":
@@ -37,30 +40,30 @@ if optimizer_name == "adam":
 elif optimizer_name == "sgd":
     optimizer = tf.keras.optimizers.SGD(learning_rate=learning_rate)"""
 
-
 optimizer = tf.keras.optimizers.Adam(config["train"]["learn_rate"])
 loss = tf.keras.losses.BinaryCrossentropy(from_logits=False)
-metric_auc = tf.keras.metrics.AUC(curve='ROC',multi_label=True, num_labels=len(config["data"]["class_names"]), from_logits=False)
+metric_auc = tf.keras.metrics.AUC(curve='ROC', multi_label=True, num_labels=len(config["data"]["class_names"]),
+                                  from_logits=False)
 metric_bin_accuracy = tf.keras.metrics.BinaryAccuracy()
-metric_f1 = tfa.metrics.F1Score(num_classes=len(config["data"]["class_names"]), threshold=config["test"]["F1_threshold"], average='macro')
+# metric_f1 = tfa.metrics.F1Score(num_classes=len(mura_config["data"]["class_names"]), threshold=mura_config["test"]["F1_threshold"], average='macro')
 
 model.compile(
     optimizer=optimizer,
     loss=loss,
-    metrics=[metric_auc, metric_bin_accuracy, metric_f1],
+    metrics=[metric_auc, metric_bin_accuracy],  # metric_f1
 )
 
 # Tensorboard Callback and config logging
-log_dir = 'logs/mura-pretraining/' + datetime.now().strftime("%Y-%m-%d--%H.%M")
+log_dir = 'logs/wrist_xray_training/' + datetime.now().strftime("%Y-%m-%d--%H.%M")
 tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
 
 config_matrix = [[k, str(w)] for k, w in config["train"].items()]
 file_writer = tf.summary.create_file_writer(log_dir)
 with file_writer.as_default():
-  tf.summary.text("config", tf.convert_to_tensor(config_matrix), step=0)
+    tf.summary.text("config", tf.convert_to_tensor(config_matrix), step=0)
 
 # Checkpoint Callback to only save best checkpoint
-checkpoint_filepath = '../../checkpoints/mura/' + datetime.now().strftime("%Y-%m-%d--%H.%M") + '/cp.ckpt'
+checkpoint_filepath = '../../checkpoints/wrist_xray/' + datetime.now().strftime("%Y-%m-%d--%H.%M") + '/cp.ckpt'
 checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
     filepath=checkpoint_filepath,
     save_weights_only=True,
@@ -97,10 +100,10 @@ model.fit(
     class_weight=class_weight
 )
 
-#Model Test
-model.load_weights(checkpoint_filepath) #best
+# Model Test
+model.load_weights(checkpoint_filepath)  # best
 result = model.evaluate(
-    dataset.ds_test, 
+    dataset.ds_test,
     batch_size=config['test']['batch_size'],
     callbacks=[tensorboard_callback])
 
@@ -108,4 +111,4 @@ result = dict(zip(model.metrics_names, result))
 print("Evaluation Result: ", result)
 result_matrix = [[k, str(w)] for k, w in result.items()]
 with file_writer.as_default():
-  tf.summary.text("evaluation", tf.convert_to_tensor(result_matrix), step=0)
+    tf.summary.text("evaluation", tf.convert_to_tensor(result_matrix), step=0)
