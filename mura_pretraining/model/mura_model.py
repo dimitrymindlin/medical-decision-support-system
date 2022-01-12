a1b9530cff32554ff4a84cec8174a68aa5e1e53f
dimi9595 <dmitry.mindlin@gmail.com>
2022-01-12 10:50:16 +0000

    Added possibility to load other models in WristPredictNet and included model specific preprocessing.

--- mura_pretraining/model/mura_model.py
+++ mura_pretraining/model/mura_model.py
@@ -1,37 +1,48 @@
 # -*- coding: utf-8 -*-
-"""Mura DenseNet model"""
+"""Mura model"""
 
 # external
 import tensorflow as tf
 
-class MuraDenseNet(tf.keras.Model):
-    """MuraDenseNet Model Class"""
+class WristPredictNet(tf.keras.Model):
+    """MuraDenseNet Model Class with various base models"""
 
-    def __init__(self, config, weigths='imagenet', train_base=False):
-        super(MuraDenseNet, self).__init__(name='MuraDenseNet')
+    def __init__(self, config, weigths='imagenet', train_base=False, model_name="densenet"):
+        super(WristPredictNet, self).__init__(name='WristPredictNet')
         self.config = config
         self._input_shape = (
             self.config['data']['image_height'],
             self.config['data']['image_width'],
             self.config['data']['image_channel']
         )
-        self.img_input = tf.keras.Input(shape=self._input_shape)
-        
-        self.base_model = tf.keras.applications.DenseNet121(
-            include_top=False,
-            input_tensor=self.img_input,
-            input_shape=self._input_shape,
-            weights=weigths,
-            pooling=config['model']['pooling'],
-            classes=len(config['data']['class_names']),
-        )
+
+        if model_name == "densenet":
+            self.base_model = tf.keras.applications.DenseNet169()
+            self.preprocessing_layer = tf.keras.applications.densenet.preprocess_input
+        elif model_name == "vgg":
+            self.base_model = tf.keras.applications.VGG19()
+            self.preprocessing_layer = tf.keras.applications.vgg19.preprocess_input
+        elif model_name == "resnet":
+            self.base_model = tf.keras.applications.ResNet50()
+            self.preprocessing_layer = tf.keras.applications.resnet50.preprocess_input
+        elif model_name == "inception":
+            self.base_model = tf.keras.applications.InceptionV3()
+            self.preprocessing_layer = tf.keras.applications.inception_v3.preprocess_input
+
+        self.base_model.include_top = False,
+        self.base_model.input_tensor = self.img_input,
+        self.base_model.input_shape = self._input_shape,
+        self.base_model.weights = weigths,
+        self.base_model.pooling = config['model']['pooling'],
+        self.base_model.classes = len(config['data']['class_names'])
         self.base_model.trainable = train_base
-        
+
+        self.img_input = tf.keras.Input(shape=self._input_shape)
         self.classifier = tf.keras.layers.Dense(len(config['data']['class_names']), activation="sigmoid", name="predictions")
-        self.last_conv_layer="bn"
 
     def call(self, inputs):
-        x = self.base_model(inputs)
+        x = self.preprocessing_layer(inputs)
+        x = self.base_model(x)
         return self.classifier(x)
 
     def model(self):
