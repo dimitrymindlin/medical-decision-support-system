48e8029d9ce0903e145beba2b6c8a826f3f791f2
dimi9595 <dmitry.mindlin@gmail.com>
2022-01-18 10:20:01 +0000

    Changed train_wrist_xray.job.sh to densenet_train

--- slurm/densenet_train1_wrist_xray.job.sh
+++ slurm/densenet_train1_wrist_xray.job.sh
@@ -3,4 +3,4 @@ source /media/compute/homes/dmindlin/.bashrc
 conda activate medical-decision-support-system
 cd /media/compute/homes/dmindlin/medical-decision-support-system
 
-python3 -m wrist_xray_finetuning.scripts.train --use_class_weights
+python3 -m wrist_xray_finetuning.scripts.train --densenet
