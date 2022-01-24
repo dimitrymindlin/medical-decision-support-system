23abca23e38b0d00369cf92f603c985aae419e39
dimi9595 <dmitry.mindlin@gmail.com>
2022-01-24 13:24:53 +0000

    Refactored paths.

--- utils/path_constants.py
+++ utils/path_constants.py
@@ -1,6 +1,5 @@
 class PathConstants:
     MURA_TENSORBOARD_PREFIX = "logs_mura"
-    MURA_TENSORBOARD_TUNING_PREFIX = "logs_mura_tuning"
     WRIST_XRAY_TENSORBOARD_PREFIX = "logs_wrist_xray"
     WRIST_XRAY_TENSORBOARD_FINETUNING_PREFIX = "logs_wrist_xray_finetuning"
-    WRIST_XRAY_TENSORBOARD_TUNING_PREFIX = "logs_wrist_xray_tuning"
+    WRIST_XRAY_TENSORBOARD_HPARAMS_PREFIX = "logs_wrist_xray_hparams"
