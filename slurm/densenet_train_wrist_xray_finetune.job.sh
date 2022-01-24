b498abbee007a40ab073ebca2fbb59a2af0c4088
dimi9595 <dmitry.mindlin@gmail.com>
2022-01-24 09:46:34 +0000

    Adapted script path for slurm job.

--- slurm/densenet_train_wrist_xray_finetune.job.sh
+++ slurm/densenet_train_wrist_xray_finetune.job.sh
@@ -3,4 +3,4 @@ source /media/compute/homes/dmindlin/.bashrc
 conda activate medical-decision-support-system
 cd /media/compute/homes/dmindlin/medical-decision-support-system
 
-python3 -m wrist_xray_finetuning.scripts.train_wrist_xray_finetuning --densenet
+python3 -m wrist_xray_finetuning.scripts.train_finetune --densenet
