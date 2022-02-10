191e48e468ab121905f0e4fff0692e550bd3d3b6
dimi9595 <dmitry.mindlin@gmail.com>
2022-02-10 12:47:38 +0100

    Changed Mura tfds to only load wrist data.

--- mura_pretraining/dataloader/mura_tfds.py
+++ mura_pretraining/dataloader/mura_tfds.py
@@ -6,9 +6,9 @@ import pandas as pd
 
 
 class MuraImages(tfds.core.GeneratorBasedBuilder):
-    VERSION = tfds.core.Version('1.0.1')
+    VERSION = tfds.core.Version('1.0.3')
     RELEASE_NOTES = {
-        '1.0.1': 'Initial release.',
+        '1.0.3': 'Initial release.',
     }
 
     def _info(self) -> tfds.core.DatasetInfo:
@@ -42,7 +42,7 @@ class MuraImages(tfds.core.GeneratorBasedBuilder):
 
     def _generate_examples(self, path):
         """Yields examples."""
-        body_parts = ["XR_HAND", "XR_FOREARM", "XR_WRIST", "XR_FINGER"]
+        body_parts = ["XR_WRIST"]
 
         # Read the input data out of the source files
         root = "/".join(path.split("/")[:-2])  # ../.. to get to root dataloader folder
