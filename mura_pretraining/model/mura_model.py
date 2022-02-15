f2fbecfe75dc29a3cdabe20a1f624b52bcadd05b
dimi9595 <dmitry.mindlin@gmail.com>
2022-02-15 10:12:41 +0100

    Added missing inputs layer.

--- mura_pretraining/model/mura_model.py
+++ mura_pretraining/model/mura_model.py
@@ -8,23 +8,24 @@ from utils.model_utils import get_model_by_name, get_input_shape_from_config, ge
 
 
 class WristPredictNet(tf.keras.Model):
-    """MuraDenseNet Model Class with various base models"""
+    """MuraNet Model Class with various base models"""
 
     def __init__(self, config, weights='imagenet', train_base=False, include_top=True):
         super(WristPredictNet, self).__init__(name='WristPredictNet')
         self.config = config
         self.include_top = include_top
         self._input_shape = get_input_shape_from_config(self.config)
-        self.img_input = tf.keras.Input(shape=self._input_shape)
+        self.inputs = tf.keras.Input(shape=self._input_shape)
         self.preprocessing_layer = get_preprocessing_by_name(self.config, self._input_shape)
         self.random_flipping_aug = tf.keras.layers.RandomFlip(mode="vertical")
         self.random_rotation_aug = tf.keras.layers.experimental.preprocessing.RandomRotation(0.3)
-        self.base_model = get_model_by_name(self.config, self.img_input, self._input_shape, weights)
+        self.base_model = get_model_by_name(self.config, self.inputs, self._input_shape, weights)
         self.base_model.trainable = train_base
         self.classifier = tf.keras.layers.Dense(len(self.config['data']['class_names']), activation="sigmoid",
                                                 name="predictions")
 
     def call(self, x):
+        x = self.inputs(x)
         x = tfa.image.equalize(x)
         x = self.resize_with_pad(x)
         x = self.preprocessing_layer(x)  # Normalisation to [0,1]
