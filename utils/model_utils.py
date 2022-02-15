c7c64577e3450d92b8e746753a74ec44025d6d0f
dimi9595 <dmitry.mindlin@gmail.com>
2022-02-15 18:22:14 +0100

    refactoring.

--- utils/model_utils.py
+++ utils/model_utils.py
@@ -1,23 +1,23 @@
 import tensorflow as tf
 
 
-def get_model_by_name(config, _input_shape, weights):
+def get_model_by_name(config, input_shape, weights):
     print(f"Loading Model: {config['model']['name']}")
     if config["model"]["name"] == "densenet":
         base_model = tf.keras.applications.DenseNet121(include_top=False,
-                                                       input_shape=_input_shape,
+                                                       input_shape=input_shape,
                                                        weights=weights,
                                                        pooling=config['model']['pooling'],
                                                        classes=len(config['data']['class_names']))
     elif config["model"]["name"] == "vgg":
         base_model = tf.keras.applications.VGG19(include_top=False,
-                                                 input_shape=_input_shape,
+                                                 input_shape=input_shape,
                                                  weights=weights,
                                                  pooling=config['model']['pooling'],
                                                  classes=len(config['data']['class_names']))
     elif config["model"]["name"] == "resnet":
         base_model = tf.keras.applications.ResNet50(include_top=False,
-                                                    input_shape=_input_shape,
+                                                    input_shape=input_shape,
                                                     weights=weights,
                                                     pooling=config['model']['pooling'],
                                                     classes=len(config['data']['class_names']))
@@ -25,7 +25,7 @@ def get_model_by_name(config, _input_shape, weights):
     else:
         # Inception
         base_model = tf.keras.applications.InceptionV3(include_top=False,
-                                                       input_shape=_input_shape,
+                                                       input_shape=input_shape,
                                                        weights=weights,
                                                        pooling=config['model']['pooling'],
                                                        classes=len(config['data']['class_names']))
