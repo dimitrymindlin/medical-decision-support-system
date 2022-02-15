f4956bbe027c0908046d4601326990a4f461f0ad
dimi9595 <dmitry.mindlin@gmail.com>
2022-02-15 18:02:51 +0100

    deleted input_tensor from models.

--- utils/model_utils.py
+++ utils/model_utils.py
@@ -1,33 +1,30 @@
 import tensorflow as tf
 
 
-def get_model_by_name(config, img_input, _input_shape, weights):
+def get_model_by_name(config, _input_shape, weights):
     print(f"Loading Model: {config['model']['name']}")
     if config["model"]["name"] == "densenet":
         base_model = tf.keras.applications.DenseNet121(include_top=False,
-                                                       input_tensor=img_input,
                                                        input_shape=_input_shape,
                                                        weights=weights,
                                                        pooling=config['model']['pooling'],
                                                        classes=len(config['data']['class_names']))
     elif config["model"]["name"] == "vgg":
         base_model = tf.keras.applications.VGG19(include_top=False,
-                                                 input_tensor=img_input,
                                                  input_shape=_input_shape,
                                                  weights=weights,
                                                  pooling=config['model']['pooling'],
                                                  classes=len(config['data']['class_names']))
     elif config["model"]["name"] == "resnet":
         base_model = tf.keras.applications.ResNet50(include_top=False,
-                                                    input_tensor=img_input,
                                                     input_shape=_input_shape,
                                                     weights=weights,
                                                     pooling=config['model']['pooling'],
                                                     classes=len(config['data']['class_names']))
 
-    elif config["model"]["name"] == "inception":
+    else:
+        # Inception
         base_model = tf.keras.applications.InceptionV3(include_top=False,
-                                                       input_tensor=img_input,
                                                        input_shape=_input_shape,
                                                        weights=weights,
                                                        pooling=config['model']['pooling'],
@@ -43,7 +40,8 @@ def get_preprocessing_by_name(config, _input_shape):
     elif config["model"]["name"] == "resnet":
         preprocessing_layer = tf.keras.applications.resnet50.preprocess_input
 
-    elif config["model"]["name"] == "inception":
+    else:
+        # Inception
         preprocessing_layer = tf.keras.applications.inception_v3.preprocess_input
     return preprocessing_layer
 
