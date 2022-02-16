1515acddf6c6a5a05ddb53679db0dfbf4c9e245f
dimi9595 <dmitry.mindlin@gmail.com>
2022-02-16 10:18:29 +0100

    Added tensorboard finetuning hparams path

--- utils/path_constants.py
+++ utils/path_constants.py
@@ -1,5 +1,6 @@
 class PathConstants:
     MURA_TENSORBOARD_PREFIX = "logs_mura"
+    MURA_TENSORBOARD_HPARAMS_PREFIX = "logs_mura_finetuning_hparams"
     WRIST_XRAY_TENSORBOARD_PREFIX = "logs_wrist_xray"
     WRIST_XRAY_TENSORBOARD_FINETUNING_PREFIX = "logs_wrist_xray_finetuning"
     WRIST_XRAY_TENSORBOARD_HPARAMS_PREFIX = "logs_wrist_xray_hparams"
