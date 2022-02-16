4e9c6f8b8bd919e9158ea937e8d6fa9e0c8e56a1
dimi9595 <dmitry.mindlin@gmail.com>
2022-02-16 10:21:18 +0100

    Added global avg pooling hparam.

--- mura_finetuning/model/finetuning_model.py
+++ mura_finetuning/model/finetuning_model.py
@@ -9,9 +9,11 @@ def get_finetuning_model_from_pretrained_model(model):
 
 
 def get_finetuning_model_from_pretrained_model_hp(model, hp):
-    """x = model.layers[-1].output
-    if hp.Boolean("dropout"):"""
-    x = tf.keras.layers.Dropout(0.2)(model.layers[-1].output)  # Regularize with dropout
+    x = model.layers[-1].output
+    if hp.Boolean("global_avg_pooling"):
+        x = tf.keras.layers.GlobalAveragePooling2D()(x)
+    if hp.Boolean("dropout"):
+        x = tf.keras.layers.Dropout(0.2)(x)  # Regularize with dropout
     x = tf.keras.layers.Dense(1, activation='sigmoid')(x)
     model = tf.keras.Model(inputs=model.layers[0].input, outputs=x)
     return model
