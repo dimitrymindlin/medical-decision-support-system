2dd2492d55687cf860bf4fe1ccd1d7ad20aff50d
dimi9595 <dmitry.mindlin@gmail.com>
2022-02-17 10:08:49 +0100

    corrected hp model.

--- mura_finetuning/model/finetuning_model.py
+++ mura_finetuning/model/finetuning_model.py
@@ -10,10 +10,12 @@ def get_finetuning_model_from_pretrained_model(model):
 
 def get_finetuning_model_from_pretrained_model_hp(model, hp):
     x = model.layers[-1].output
-    if hp.Boolean("global_avg_pooling"):
+    dropout_rate = hp.Choice(0.2, 0.3)
+    if hp.Boolean("extra_layers"):
         x = tf.keras.layers.GlobalAveragePooling2D()(x)
-    if hp.Boolean("dropout"):
-        x = tf.keras.layers.Dropout(0.2)(x)  # Regularize with dropout
+        x = tf.keras.layers.Dropout(dropout_rate)(x)  # Regularize with dropout
+    elif hp.Boolean("dropout"):
+        x = tf.keras.layers.Dropout(dropout_rate)(x)  # Regularize with dropout
     x = tf.keras.layers.Dense(1, activation='sigmoid')(x)
     model = tf.keras.Model(inputs=model.layers[0].input, outputs=x)
     return model
