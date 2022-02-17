707550a808bc8982a6241ff85cece4de2d161bcf
dimi9595 <dmitry.mindlin@gmail.com>
2022-02-17 12:37:32 +0100

    Adapted tensorboard to new paths.

--- slurm/tensorboard_wrist_train.job.sh
+++ slurm/tensorboard_wrist_train.job.sh
@@ -4,7 +4,7 @@ conda activate medical-decision-support-system
 
 cd /media/compute/homes/dmindlin/medical-decision-support-system
 
-tensorboard dev upload --logdir logs_wrist_train \
-    --name "Mura Wrist Last Layers Board" \
-    --description "Comparison of several hyperparameters and pretraining" \
+tensorboard dev upload --logdir logs_frozen \
+    --name "MDS Frozen" \
+    --description "Training last layers the pretrained model of XR-Wrist" \
     --one_shot
