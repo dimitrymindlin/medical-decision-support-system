67444f033e14c604e7f1438ddbb6bebb2c1ad4d1
dimi9595 <dmitry.mindlin@gmail.com>
2022-02-17 10:17:09 +0100

    corrected hp model removed global avg pooling.

--- mura_finetuning/model/finetuning_model.py
+++ mura_finetuning/model/finetuning_model.py
@@ -11,10 +11,7 @@ def get_finetuning_model_from_pretrained_model(model):
 def get_finetuning_model_from_pretrained_model_hp(model, hp):
     x = model.layers[-1].output
     dropout_rate = hp.Choice('dropout_rate', [0.2, 0.3])
-    if hp.Boolean("extra_layers"):
-        x = tf.keras.layers.GlobalAveragePooling2D()(x)
-        x = tf.keras.layers.Dropout(dropout_rate)(x)  # Regularize with dropout
-    elif hp.Boolean("dropout"):
+    if hp.Boolean("dropout"):
         x = tf.keras.layers.Dropout(dropout_rate)(x)  # Regularize with dropout
     x = tf.keras.layers.Dense(1, activation='sigmoid')(x)
     model = tf.keras.Model(inputs=model.layers[0].input, outputs=x)
