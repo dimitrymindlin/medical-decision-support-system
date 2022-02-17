60d70e3912405c323b097a8c000dc5bb0c8b9fa4
dimi9595 <dmitry.mindlin@gmail.com>
2022-02-17 16:20:11 +0100

    Corrected name for tensorboard.

--- slurm/tensorboard_frozen_hp.job.sh
+++ slurm/tensorboard_frozen_hp.job.sh
@@ -5,6 +5,6 @@ conda activate medical-decision-support-system
 cd /media/compute/homes/dmindlin/medical-decision-support-system
 
 tensorboard dev upload --logdir tensorboard_logs/logs_frozen_hp \
-    --name "Mura Wrist Last Layers HP Board" \
-    --description "Comparison of several hyperparameters and pretraining" \
+    --name "MDS FROZEN HP" \
+    --description "Comparison of several hyperparameters on the pretrained model for XR-WRIST training" \
     --one_shot
