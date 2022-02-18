8c00062fe0515327021e9d9da0c75fa0f562e727
dimi9595 <dmitry.mindlin@gmail.com>
2022-02-18 15:25:21 +0100

    Added tensorboard image tests.

---
+++ slurm/image_test.job.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+source /media/compute/homes/dmindlin/.bashrc
+conda activate medical-decision-support-system
+cd /media/compute/homes/dmindlin/medical-decision-support-system
+
+python3 -m utils.image_sumaries
---
+++ slurm/tensorboard_image_test.job.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+source /media/compute/homes/dmindlin/.bashrc
+conda activate medical-decision-support-system
+
+cd /media/compute/homes/dmindlin/medical-decision-support-system
+
+tensorboard dev upload --logdir logs \
+    --name "Images Test" \
+    --description "Testing..." \
+    --one_shot
