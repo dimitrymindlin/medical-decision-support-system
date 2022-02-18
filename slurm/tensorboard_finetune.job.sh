cbacfdad5673bfc0d740dd56154778c93781f283
dimi9595 <dmitry.mindlin@gmail.com>
2022-02-18 14:43:46 +0100

    trying bind_all argument.

--- slurm/tensorboard_finetune.job.sh
+++ slurm/tensorboard_finetune.job.sh
@@ -5,6 +5,8 @@ conda activate medical-decision-support-system
 cd /media/compute/homes/dmindlin/medical-decision-support-system
 
 tensorboard dev upload --logdir tensorboard_logs/logs_finetune \
+    --bind_all
     --name "MDS Finetuning Board" \
     --description "Final Finetuning of all layers on XR-Wrist" \
     --one_shot
+
