b44a8f251debd1e3ef68b68f4c4e2e9131aee5e9
dimi9595 <dmitry.mindlin@gmail.com>
2022-02-21 11:48:29 +0100

    get_fancy_mura_model changes.

--- models/mura_model.py
+++ models/mura_model.py
@@ -85,8 +85,8 @@ def get_sequential_model(config, weights='imagenet', include_top=True):
 def get_fancy_mura_model(config):
     input_shape = get_input_shape_from_config(config)
     inputs = tf.keras.Input(shape=input_shape)
-    pre = PreprocessNet(config)(inputs)
-    wrist_net = WristPredictNet(config, include_top=False)(pre)
+    #pre = PreprocessNet(config)(inputs)
+    wrist_net = WristPredictNet(config, include_top=False)(inputs)
     x = tf.keras.layers.GlobalAveragePooling2D()(wrist_net)
 
     x = tf.keras.layers.Dense(1024)(x)  ###
