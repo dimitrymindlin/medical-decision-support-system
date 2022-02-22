391f15f933b14aa97b6186eacf4d2cd978b56c24
dimi9595 <dmitry.mindlin@gmail.com>
2022-02-22 08:20:56 +0100

    Added tensorboard kaggle.

---
+++ slurm/tensorboard_kaggle.job.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+source /media/compute/homes/dmindlin/.bashrc
+conda activate medical-decision-support-system
+
+cd /media/compute/homes/dmindlin/medical-decision-support-system
+
+tensorboard dev upload --logdir logs/fit \
+    --name "MDS Pretrain" \
+    --description "Pretraining models on the mura dataset except of XR-WRIST" \
+    --one_shot
