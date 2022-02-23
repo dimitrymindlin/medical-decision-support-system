dd0c798555f29c92153263a324376c134788fcd9
dimi9595 <dmitry.mindlin@gmail.com>
2022-02-23 16:44:26 +0100

    Adapted train_kaggle

--- mura_pretraining/scripts/train_kaggle.py
+++ mura_pretraining/scripts/train_kaggle.py
@@ -5,7 +5,6 @@ import tensorflow as tf
 import tensorflow_addons as tfa
 import numpy as np
 
-# Get Dataset
 from sklearn.metrics import confusion_matrix, classification_report, cohen_kappa_score
 from configs.direct_training_config import direct_training_config as config
 from mura_pretraining.dataloader import MuraDataset
@@ -16,6 +15,40 @@ TF_LOG_DIR = f'kaggle/kaggle_{model_name}/' + timestamp + "/"
 checkpoint_filepath = f'checkpoints/kaggle_{model_name}/' + timestamp + '/cp.ckpt'
 
 dataset = MuraDataset(config, only_wrist_data=True)
+# Checkpoint Callback to only save best checkpoint
+
+my_callbacks = [
+    keras.callbacks.ModelCheckpoint(filepath=checkpoint_filepath,
+                                    # Callback to save the Keras model or model weights at some frequency.
+                                    monitor='val_accuracy',
+                                    verbose=0,
+                                    save_best_only=True,
+                                    save_weights_only=False,
+                                    mode='auto',
+                                    save_freq='epoch'),
+    keras.callbacks.ReduceLROnPlateau(monitor='val_accuracy',
+                                      # Reduce learning rate when a metric has stopped improving.
+                                      factor=0.1,
+                                      patience=3,
+                                      min_delta=0.001,
+                                      verbose=1,
+                                      min_lr=0.000000001),
+    keras.callbacks.TensorBoard(log_dir=TF_LOG_DIR,
+                                histogram_freq=1,
+                                write_graph=True,
+                                write_images=False,
+                                update_freq='epoch',
+                                profile_batch=30,
+                                embeddings_freq=1,
+                                embeddings_metadata=None
+                                ),
+    keras.callbacks.EarlyStopping(monitor="val_accuracy",
+                                  patience=5,
+                                  mode="max",
+                                  baseline=None,
+                                  restore_best_weights=True,
+                                  )
+]
 
 base_model = keras.applications.InceptionV3(
     input_shape=(224, 224, 3),
@@ -23,7 +56,6 @@ base_model = keras.applications.InceptionV3(
 
 # Create a new model on top
 input_image = keras.layers.Input((224, 224, 3))
-#x = tf.keras.applications.inception_v3.preprocess_input(input_image)  # Normalisation to [0,1]
 x = base_model(input_image)
 
 # Convert features of shape `base_model.output_shape[1:]` to vectors
@@ -44,61 +76,25 @@ model = keras.Model(inputs=input_image, outputs=out)
 metric_auc = tf.keras.metrics.AUC(curve='ROC', multi_label=True, num_labels=2, from_logits=False)
 
 
-model.compile(optimizer=keras.optimizers.Adam(lr=0.0001),
+model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.0001),
               loss='categorical_crossentropy',
               metrics=["accuracy", metric_auc])
-
-# Tensorboard Callback and config logging
-tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=TF_LOG_DIR)
-
-# Checkpoint Callback to only save best checkpoint
-
-checkpoint_clb = keras.callbacks.ModelCheckpoint(filepath=checkpoint_filepath,
-                                                 # Callback to save the Keras model or model weights at some frequency.
-                                                 monitor='val_accuracy',
-                                                 verbose=0,
-                                                 save_best_only=True,
-                                                 save_weights_only=True,
-                                                 mode='auto',
-                                                 save_freq='epoch'),
-reduce_on_plt_clb = keras.callbacks.ReduceLROnPlateau(monitor='val_accuracy',
-                                                      # Reduce learning rate when a metric has stopped improving.
-                                                      factor=0.1,
-                                                      patience=3,
-                                                      min_delta=0.001,
-                                                      verbose=1,
-                                                      min_lr=0.000000001),
-early_stopping_clk = keras.callbacks.EarlyStopping(monitor="val_accuracy",
-                                                   patience=5,
-                                                   mode="max",
-                                                   baseline=None,
-                                                   restore_best_weights=True,
-                                                   )
 
 # Class weights for training underrepresented classes
 class_weight = dataset.train_classweights if config["train"]["use_class_weights"] else None
 
 # Model Training
 # model.load_weights("checkpoints/kaggle_inception/2022-02-21--15.47/cp.ckpt")
-model.fit(
+history = model.fit(
     dataset.ds_train,
     epochs=config["train"]["epochs"],
     validation_data=dataset.ds_test,
-    callbacks=[tensorboard_callback, checkpoint_clb, reduce_on_plt_clb, early_stopping_clk],
+    callbacks=my_callbacks,
+    verbose=1,
     class_weight=class_weight
 )
 
-# Model Test
-model.load_weights(checkpoint_filepath)  # best
-result = model.evaluate(
-    dataset.ds_test,
-    batch_size=config['test']['batch_size'],
-    callbacks=[tensorboard_callback])
-
-result = dict(zip(model.metrics_names, result))
-print("Evaluation Result: ", result)
-result_matrix = [[k, str(w)] for k, w in result.items()]
-
+print(history.history)
 print("Kaggel Test Evaluation")
 m = tfa.metrics.CohenKappa(num_classes=2, sparse_labels=False)
 y_true_2d = np.concatenate([y for x, y in dataset.ds_test], axis=0)
@@ -106,7 +102,7 @@ y_pred_2d = model.predict(dataset.ds_test)
 y_pred_1d = np.argmax(y_pred_2d, axis=1)
 y_true_1d = np.argmax(y_true_2d, axis=1)
 print(y_pred_2d.shape, y_true_2d.shape)
-m.update_state(y_true=y_true_1d, y_pred=y_pred_1d)
+m.update_state(y_true_1d, y_pred_1d)
 print('Final result: ', m.result().numpy())
 print("SKlearn kappa ", cohen_kappa_score(y_true_1d, y_pred_1d))
 print(y_true_1d.shape)
@@ -114,3 +110,24 @@ print(y_pred_1d.shape)
 cm = confusion_matrix(y_true_1d, y_pred_1d)
 print(cm)
 print(classification_report(y_true_1d, y_pred_1d))
+
+print(history.history)
+print("Test Evaluation")
+m = tfa.metrics.CohenKappa(num_classes=2, sparse_labels=False)
+y_pred_valid_2d = model.predict(dataset.ds_test)
+y_pred_valid_1d = np.argmax(y_pred_valid_2d, axis=1)
+y_true_valid_2d = np.concatenate([y for x, y in dataset.ds_test], axis=0)
+y_true_valid_1d = np.argmax(y_pred_valid_2d, axis=1)
+print(y_pred_valid_2d.shape, y_true_valid_2d.shape)
+m.update_state(y_true_valid_1d, y_pred_valid_1d)
+print('Final result: ', m.result().numpy())
+print("Sklearn kappa ", cohen_kappa_score(y_true_valid_1d, y_pred_valid_1d))
+print(confusion_matrix(y_true_valid_1d, y_pred_valid_1d))
+print(classification_report(y_true_valid_1d, y_pred_valid_1d))
+print("Train Evaluation")
+y_pred_train_2d = model.predict(dataset.ds_train)
+y_pred_train_1d = np.argmax(y_pred_train_2d, axis=1)
+y_true_train_1d = np.concatenate([y for x, y in dataset.ds_train], axis=0)
+print(confusion_matrix(y_pred_train_1d, y_pred_train_1d))
+print(classification_report(y_pred_train_1d, y_true_train_1d))
+print("Sklearn kappa ", cohen_kappa_score(y_true_train_1d, y_pred_train_1d))
