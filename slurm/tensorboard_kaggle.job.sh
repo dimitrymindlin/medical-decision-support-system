a9b5ec8197a0c609950b5e55802b35e796e658c0
dimi9595 <dmitry.mindlin@gmail.com>
2022-03-04 14:05:25 +0100

    Adapted path.

--- slurm/tensorboard_kaggle.job.sh
+++ slurm/tensorboard_kaggle.job.sh
@@ -4,7 +4,7 @@ conda activate medical-decision-support-system
 
 cd /media/compute/homes/dmindlin/medical-decision-support-system
 
-tensorboard dev upload --logdir logs/fit \
+tensorboard dev upload --logdir kaggle \
     --name "MDS Pretrain" \
     --description "Pretraining models on the mura dataset except of XR-WRIST" \
     --one_shot
