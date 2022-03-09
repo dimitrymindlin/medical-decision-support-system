d71901697360e19ab7aa0fa2fd1c39bc0f51a09a
dimi9595 <dmitry.mindlin@gmail.com>
2022-03-09 08:46:03 +0100

    CHanged paths to gpu.

--- mura_finetuning/dataloader/mura_generators.py
+++ mura_finetuning/dataloader/mura_generators.py
@@ -109,14 +109,14 @@ class MuraValidDataGenerator(Sequence):
 def get_mura_loaders(config, batch_size=32):
     # To get the filenames for a task
     def filenames(parts: List[str], train=True):
-        # root = '../tensorflow_datasets/downloads/cjinny_mura-v11/'
-        root = '/Users/dimitrymindlin/tensorflow_datasets/downloads/cjinny_mura-v11/'
+        root = '../tensorflow_datasets/downloads/cjinny_mura-v11/'
+        #root = '/Users/dimitrymindlin/tensorflow_datasets/downloads/cjinny_mura-v11/'
         if train:
-            # csv_path = "../tensorflow_datasets/downloads/cjinny_mura-v11/MURA-v1.1/train_image_paths.csv"
-            csv_path = "/Users/dimitrymindlin/tensorflow_datasets/downloads/cjinny_mura-v11/MURA-v1.1/train_image_paths.csv"
+            csv_path = "../tensorflow_datasets/downloads/cjinny_mura-v11/MURA-v1.1/train_image_paths.csv"
+            #csv_path = "/Users/dimitrymindlin/tensorflow_datasets/downloads/cjinny_mura-v11/MURA-v1.1/train_image_paths.csv"
         else:
-            # csv_path = "../tensorflow_datasets/downloads/cjinny_mura-v11/MURA-v1.1/valid_image_paths.csv"
-            csv_path = "/Users/dimitrymindlin/tensorflow_datasets/downloads/cjinny_mura-v11/MURA-v1.1/valid_image_paths.csv"
+            csv_path = "../tensorflow_datasets/downloads/cjinny_mura-v11/MURA-v1.1/valid_image_paths.csv"
+            #csv_path = "/Users/dimitrymindlin/tensorflow_datasets/downloads/cjinny_mura-v11/MURA-v1.1/valid_image_paths.csv"
 
         with open(csv_path, 'rb') as F:
             d = F.readlines()
