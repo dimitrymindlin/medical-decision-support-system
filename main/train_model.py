1ab2fec5aa3c7d3921db4ee3a49c88a9c1b864a9
dimi9595 <dmitry.mindlin@gmail.com>
2022-03-11 08:20:41 +0100

    Corregtes sys args.

--- main/train_model.py
+++ main/train_model.py
@@ -6,17 +6,17 @@ from mura_finetuning.scripts.training_routine import train_model
 
 config = None
 for arg in sys.argv:
-    if arg == "pretrain":
+    if arg == "--pretrain":
         config = pretraining_config
         print("Using pretrain config")
         break
-    elif arg == "frozen":  # Freeze base and train last layers
+    elif arg == "--frozen":  # Freeze base and train last layers
         config = finetuning_config
         config["train"]["prefix"] = "frozen"
         config["train"]["train_base"] = False
         config["train"]["learning_rate"]: 0.001
         break
-    elif arg == "finetune":  # Finetune whole model with low lr
+    elif arg == "--finetune":  # Finetune whole model with low lr
         config = finetuning_config
         config["train"]["prefix"] = "finetune"
         config["train"]["train_base"] = True
