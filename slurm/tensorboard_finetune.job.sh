51bf566ecc97ae6973decd2455034e2e05e8c104
dimi9595 <dmitry.mindlin@gmail.com>
2022-03-12 18:34:04 +0100

    corrected logs name.

--- slurm/tensorboard_finetune.job.sh
+++ slurm/tensorboard_finetune.job.sh
@@ -4,7 +4,7 @@ conda activate medical-decision-support-system
 
 cd /media/compute/homes/dmindlin/medical-decision-support-system
 
-tensorboard dev upload --logdir tensorboard_logs/logs_finetune \
+tensorboard dev upload --logdir tensorboard_logs/logs_finetuning \
     --name "MDS Finetuning Board" \
     --description "Final Finetuning of all layers on XR-Wrist" \
     --one_shot
