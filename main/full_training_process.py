272655f385eaabb64593846d2ada22f7f331d5ec
dimi9595 <dmitry.mindlin@gmail.com>
2022-03-20 21:13:38 +0100

    changed batch size for inception.

--- main/full_training_process.py
+++ main/full_training_process.py
@@ -23,13 +23,17 @@ configs = [pretraining_config, frozen_config, finetuning_config]
 
 last_stage = None
 last_saved_model_name = None
+batch_size = 16
 
 if model_name == "inception":
     configs = configs[1:]
     last_saved_model_timestamp = "2022-03-19--10.48"
+    batch_size = 32
 
 for conf in configs:
     conf["model"]["name"] = model_name
+    conf["train"]["batch_size"] = batch_size
+    conf["test"]["batch_size"] = batch_size
     if conf["train"]["prefix"] != "pretrain":
         conf["train"]["checkpoint_name"] = last_saved_model_timestamp
     last_saved_model_timestamp = train_model(conf, print_console=False)
