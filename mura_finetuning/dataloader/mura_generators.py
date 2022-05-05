31c55b9501c99733f5c0ee85b8664df89fa9e456
dimi9595 <dmitry.mindlin@gmail.com>
2022-05-05 16:37:58 +0200

    Added Test_img_data_generator to generate selected images.

--- mura_finetuning/dataloader/mura_generators.py
+++ mura_finetuning/dataloader/mura_generators.py
@@ -26,7 +26,7 @@ class MuraGeneratorDataset():
             CLAHE(always_apply=True)
         ])
         self.preprocess_img = preprocess_img
-        self.train_loader, self.valid_loader, self.test_loader, self.raw_valid_loader, self.train_y, self.test_y = get_mura_loaders(
+        self.train_loader, self.valid_loader, self.test_loader, self.selected_test_loader, self.train_y, self.test_y = get_mura_loaders(
             config,
             batch_size=self.config["train"]["batch_size"],
             aug_train=self.AUGMENTATIONS_TRAIN,
@@ -108,23 +108,86 @@ class MuraValidDataGenerator(Sequence):
         ys = np.reshape(ys, (-1, 2))
         return xs, ys
 
+class Test_img_data_generator(Sequence):
+    """
+    Take 15 positive self selected examples and try counterfactual generation
+    """
+    def __init__(self, batch_size, input_size=(512, 512), transform=None):
+        self.input_size = input_size
+        self.t = transform
+        #root = "../tensorflow_datasets/downloads/cjinny_mura-v11/MURA-v1.1/valid/XR_WRIST/"
+        root = '/Users/dimitrymindlin/tensorflow_datasets/downloads/cjinny_mura-v11/MURA-v1.1/valid/XR_WRIST/'
+        """
+                                root + "patient11186/study3_positive/image3.png",
+                                root + "patient11188/study1_positive/image1.png",
+                                root + "patient11188/study1_positive/image2.png",
+                                root + "patient11188/study1_positive/image3.png",
+                                root + "patient11188/study1_positive/image4.png",
+                                root + "patient11190/study1_positive/image1.png",
+                                root + "patient11190/study1_positive/image2.png",
+                                root + "patient11186/study2_positive/image2.png",
+                                root + "patient11192/study1_positive/image1.png",
+                                root + "patient11192/study1_positive/image2.png",
+                                root + "patient11192/study1_positive/image3.png"
+        """
+        self.pos_image_paths = [root + "patient11186/study2_positive/image1.png",
+                                root + "patient11186/study2_positive/image3.png",
+                                root + "patient11186/study3_positive/image1.png",
+                                root + "patient11186/study3_positive/image2.png",
+                                root + "patient11205/study1_positive/image1.png",
+                                root + "patient11205/study1_positive/image2.png",
+                                root + "patient11205/study1_positive/image3.png",
+                                root + "patient11267/study1_positive/image1.png"]
+        self.batch_size = len(self.pos_image_paths)
+
+    def __len__(self):
+        return (np.ceil(len(self.pos_image_paths) / float(self.batch_size))).astype(np.int)
+
+    def __getitem__(self, idx):
+        # TODO: ONLY FOR BATCH SIZE OF 1
+        batch_pos = self.pos_image_paths[idx * self.batch_size: (idx + 1) * self.batch_size]
+        y = np.array([0, 1])
+        batches = [batch_pos]
+        pos = []
+        ys = []
+        for i, batch in enumerate(batches):
+            for file in batch:
+                img = imread(file)
+                if self.t:
+                    img = self.t(image=img)["image"]
+                if len(img.shape) < 3:
+                    img = tf.expand_dims(img, axis=-1)
+                if img.shape[-1] != 3:
+                    img = tf.image.grayscale_to_rgb(img)
+                img = tf.image.resize_with_pad(img, self.input_size[0], self.input_size[1])
+                pos.append(img)
+                ys.append(y)
+        pos = tf.stack(pos)
+        ys = np.reshape(ys, (-1, 2))
+        return pos, ys
+
 
 def get_mura_loaders(config, batch_size=32, aug_train=None, aug_test=None):
     # To get the filenames for a task
     def filenames(parts: List[str], train=True):
-        root = '../tensorflow_datasets/downloads/cjinny_mura-v11/'
-        # root = '/Users/dimitrymindlin/tensorflow_datasets/downloads/cjinny_mura-v11/'
+        #root = '../tensorflow_datasets/downloads/cjinny_mura-v11/'
+        root = '/Users/dimitrymindlin/tensorflow_datasets/downloads/cjinny_mura-v11/'
         if train:
-            csv_path = "../tensorflow_datasets/downloads/cjinny_mura-v11/MURA-v1.1/train_image_paths.csv"
-            # csv_path = "/Users/dimitrymindlin/tensorflow_datasets/downloads/cjinny_mura-v11/MURA-v1.1/train_image_paths.csv"
+            #csv_path = "../tensorflow_datasets/downloads/cjinny_mura-v11/MURA-v1.1/train_image_paths.csv"
+            csv_path = "/Users/dimitrymindlin/tensorflow_datasets/downloads/cjinny_mura-v11/MURA-v1.1/train_image_paths.csv"
+            #csv_path = "/Users/dimitrymindlin/tensorflow_datasets/downloads/cjinny_mura-v11/MURA-v1.1_transformed/train_image_paths.csv"
+            #csv_path = "../tensorflow_datasets/downloads/cjinny_mura-v11/MURA-v1.1_transformed/train_image_paths.csv"
         else:
-            csv_path = "../tensorflow_datasets/downloads/cjinny_mura-v11/MURA-v1.1/valid_image_paths.csv"
-            # csv_path = "/Users/dimitrymindlin/tensorflow_datasets/downloads/cjinny_mura-v11/MURA-v1.1/valid_image_paths.csv"
+            #csv_path = "../tensorflow_datasets/downloads/cjinny_mura-v11/MURA-v1.1/valid_image_paths.csv"
+            csv_path = "/Users/dimitrymindlin/tensorflow_datasets/downloads/cjinny_mura-v11/MURA-v1.1/valid_image_paths.csv"
+            #csv_path = "/Users/dimitrymindlin/tensorflow_datasets/downloads/cjinny_mura-v11/MURA-v1.1_transformed/valid_image_paths.csv"
+            #csv_path = "../tensorflow_datasets/downloads/cjinny_mura-v11/MURA-v1.1_transformed/valid_image_paths.csv"
 
         with open(csv_path, 'rb') as F:
             d = F.readlines()
-            imgs = [root + str(x, encoding='utf-8').strip() for x in d if
+            imgs = [root + str(x, encoding='utf-8').strip().replace("MURA-v1.1", "MURA-v1.1_transformed") for x in d if
                     str(x, encoding='utf-8').strip().split('/')[2] in parts]
+            #imgs = [img_path.replace("MURA-v1.1", "MURA-v1.1_transformed") for img_path in imgs]
 
         # imgs= [x.replace("/", "\\") for x in imgs]
         labels = [x.split('_')[-1].split('/')[0] for x in imgs]
@@ -146,13 +209,14 @@ def get_mura_loaders(config, batch_size=32, aug_train=None, aug_test=None):
     train_gen = MuraGenerator(config, train_x, train_y, batch_size, aug_train)
     valid_gen = MuraGenerator(config, valid_x, valid_y, batch_size, aug_test)
     test_gen = MuraGenerator(config, test_x, test_y, batch_size, aug_test)
-    test_raw_gen = MuraValidDataGenerator(config, test_x, test_y, transform=aug_test)
+    #test_raw_gen = MuraValidDataGenerator(config, test_x, test_y, transform=aug_test)
+    seleted_test_gen = Test_img_data_generator(batch_size=batch_size, input_size=config["data"]["input_size"], transform=aug_test)
 
     print(f"Train data amount: {len(train_y)}")
     print(f"Valid data amount: {len(valid_y)}")
     print(f"Test data amount: {len(test_y)}")
 
-    return train_gen, valid_gen, test_gen, test_raw_gen, train_y, test_y
+    return train_gen, valid_gen, test_gen, seleted_test_gen, train_y, test_y
 
 
 def preprocess_img(img, model_name="inception"):
