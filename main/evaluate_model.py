91f9de6d96c6874ae22a0692ef0d226084ab38fa
dimi9595 <dmitry.mindlin@gmail.com>
2022-08-08 14:00:48 +0200

    Added arguments for script.

--- main/evaluate_model.py
+++ main/evaluate_model.py
@@ -1,7 +1,29 @@
+import sys
+
+from configs.direct_training_config import direct_training_config
+from configs.finetuning_config import finetuning_config
+from configs.frozen_config import frozen_config
+from configs.pretraining_config import pretraining_config
 from dataloader.mura_wrist_dataset import MuraDataset
 from utils.eval_metrics import log_and_pring_evaluation
 import tensorflow_addons as tfa
 import tensorflow as tf
+
+for arg in sys.argv:
+    if arg == "--pretrain":
+        config = pretraining_config
+        break
+    elif arg == "--frozen":  # Freeze base and train last layers
+        config = frozen_config
+        break
+    elif arg == "--finetuning":  # Finetune whole model with low lr
+        config = finetuning_config
+        break
+    elif arg == "--direct":
+        config = direct_training_config
+        break
+    if arg.startswith("2022"):
+        clf_path = arg
 
 
 def evaluate_model(config, clf_path):
@@ -15,3 +37,6 @@ def evaluate_model(config, clf_path):
     mura_data = MuraDataset(config)
 
     log_and_pring_evaluation(model, mura_data, config, None)
+    
+
+evaluate_model(config, clf_path)
--- slurm/evaluate_model.job.sh
+++ slurm/evaluate_model.job.sh
@@ -3,4 +3,4 @@ source /media/compute/homes/dmindlin/.bashrc
 conda activate medical-decision-support-system
 cd /media/compute/homes/dmindlin/medical-decision-support-system
 
-python3 -m main.evaluate_model --2022-08-08--10.09
+python3 -m main.evaluate_model --direct --2022-08-08--10.09
