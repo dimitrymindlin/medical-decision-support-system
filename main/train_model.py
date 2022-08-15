85bb8279a3ca8aaebbd47d5055406e76de2136a8
dimi9595 <dmitry.mindlin@gmail.com>
2022-08-15 17:21:50 +0200

    Revert "fixed gpu path."
    This reverts commit 3fe921bb2113b8a76e5dc5651da9d0943629f3a2.

--- main/train_model.py
+++ main/train_model.py
@@ -47,7 +47,6 @@ print(f'Using {config["train"]["prefix"]} config')"""
 if len(tf.config.list_physical_devices('GPU')) == 0:
     TFDS_PATH = "/Users/dimitrymindlin/tensorflow_datasets"
 else:
-    pass
-TFDS_PATH = "../tensorflow_datasets"
+    TFDS_PATH = "../tensorflow_datasets"
 config["data"]["tfds_path"] = TFDS_PATH
 train_model(config)
